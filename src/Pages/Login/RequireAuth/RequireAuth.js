554adc81bcaa4d30e221edc7c8b69a345c394335
dev-nayandas <rich.nayan@gmail.com>
2022-04-18 18:40:43 +0600

    Redirect handled

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import Loding from '../Loading/Loding';
 
 const RequireAuth = ({children}) => {
-    const [user] = useAuthState(auth);
-    const location = useLocation()
+    const [user, loading] = useAuthState(auth);
+    const location = useLocation();
+    if(loading){
+        return <Loding></Loding>
+    }
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate()
+    if (error) {
+        return (
+          <div>
+            <p>Error: {error.message}</p>
+          </div>
+        );
+      }
+      if(user){
+          navigate('/home')
+      }
     return (
         <div>
             <p className='mx-auto'>Or</p>
-            <button className='btn btn-primary'>Sign In With Google</button>
+            <button onClick={()=> signInWithGoogle()} className='btn btn-primary'>Sign In With Google</button>
         </div>
     );
 };
